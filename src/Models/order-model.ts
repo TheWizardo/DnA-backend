bdb2850370d9a68ca8249d55ae741bc148bfa95d
TheWizard <oz.sabbag@gmail.com>
2024-02-26 23:52:27 +0200

    removing unnecessary members

--- src/Models/order-model.ts
+++ src/Models/order-model.ts
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 class OrderModel {
-    public id: string;
+    public order_number: string;
     public type: string;
 
     public first_name: string;
@@ -17,21 +17,19 @@ class OrderModel {
     public city: string;
     public zip: string;
 
-    public reply_to: string;
+    public email: string;
 
     public comments: string;
 
     public price: number;
 
-    public dedicate: boolean;
-    public for_self: boolean;
     public dedication_name: string;
 
     public tracking_number: string;
 
 
     public constructor(order: OrderModel) {
-        this.id = order.id;
+        this.order_number = order.order_number;
         this.type = order.type;
         this.first_name = order.first_name;
         this.last_name = order.last_name;
@@ -42,17 +40,15 @@ class OrderModel {
         this.apartment = order.apartment;
         this.city = order.city;
         this.zip = order.zip;
-        this.reply_to = order.reply_to;
+        this.email = order.email;
         this.comments = order.comments;
         this.price = order.price;
-        this.dedicate = order.dedicate;
-        this.for_self = order.for_self;
         this.dedication_name = order.dedication_name;
         this.tracking_number = order.tracking_number;
     }
 
     private static validationScheme = Joi.object({
-        id: Joi.string().required(),
+        order_number: Joi.string().optional().length(8),
         type: Joi.string().required(),
         first_name: Joi.string().required().min(2).max(50),
         last_name: Joi.string().required().min(2).max(50),
@@ -63,12 +59,10 @@ class OrderModel {
         apartment: Joi.alternatives().conditional("type", { is: "PrintedBook", then: Joi.number().positive().integer().required(), otherwise: Joi.number().optional() }),
         city: Joi.alternatives().conditional("type", { is: "PrintedBook", then: Joi.string().required(), otherwise: Joi.string().optional() }),
         zip: Joi.alternatives().conditional("type", { is: "PrintedBook", then: Joi.string().length(7).required(), otherwise: Joi.string().optional() }),
-        reply_to: Joi.string().email().required(),
+        email: Joi.string().email().required(),
         comments: Joi.string().optional().allow(null, ''),
         price: Joi.number().positive(),
-        dedicate: Joi.alternatives().conditional("type", { is: "PrintedBook", then: Joi.boolean().required(), otherwise: Joi.boolean().optional() }),
-        for_self: Joi.alternatives().conditional("dedicate", { is: true, then: Joi.boolean().required(), otherwise: Joi.boolean().optional() }),
-        dedication_name: Joi.alternatives().conditional("dedicate", { is: true, then: Joi.alternatives().conditional("for_self", { is: true, then: Joi.string().required(), otherwise: Joi.string().optional() }), otherwise: Joi.string().optional() }),
+        dedication_name: Joi.alternatives().conditional("type", { is: "PrintedBook", then: Joi.string().required().min(2).max(50), otherwise: Joi.string().optional() }),
         tracking_number: Joi.string().optional()
     });
 
