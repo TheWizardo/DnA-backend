2d25b21a8a51cea9e801e0f914fa60da7b270ac9
TheWizard <oz.sabbag@gmail.com>
2024-02-27 19:37:41 +0200

    added a file to handle future updates

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Backend for the demons-and-angels project
+
+## How to upload changes
+1. run ```npm run build``` (Make sure the ```package.json``` file and the ```Assets``` folder gets copied correctly.)
+2. commit your changes.
+3. ssh into the ec2 instance using the command ```ssh -i <pem_file_path> ec2-user@<public_dns_name>
+4. run ```update.sh``` as root.
---
+++ update.sh
@@ -0,0 +1,41 @@
+now=$(date +"%Y-%m-%dT%H:%M:%S")
+echo "[$now] stopping pm2 service" > DnA-backend/logs/backend.log
+
+pm2 stop backend
+
+now=$(date +"%Y-%m-%dT%H:%M:%S")
+echo "[$now] pm2 service stopped" >> DnA-backend/logs/backend.log
+
+
+now=$(date +"%Y-%m-%dT%H:%M:%S")
+echo "[$now] deleteing typescript source" >> DnA-backend/logs/backend.log
+
+rm -rf DnA-backend/src
+
+now=$(date +"%Y-%m-%dT%H:%M:%S")
+echo "[$now] typescript source deleted" >> DnA-backend/logs/backend.log
+
+
+now=$(date +"%Y-%m-%dT%H:%M:%S")
+echo "[$now] changing log files permissions" >> DnA-backend/logs/backend.log
+
+chmod 666 DnA-backend/logs/backend.log
+
+now=$(date +"%Y-%m-%dT%H:%M:%S")
+echo "[$now] log files permissions chnaged" >> DnA-backend/logs/backend.log
+
+
+now=$(date +"%Y-%m-%dT%H:%M:%S")
+echo "[$now] installing dependencies" >> DnA-backend/logs/backend.log
+
+cd DnA-backend/build
+npm i
+
+now=$(date +"%Y-%m-%dT%H:%M:%S")
+echo "[$now] dependencies installed" >> DnA-backend/logs/backend.log
+
+
+now=$(date +"%Y-%m-%dT%H:%M:%S")
+echo "[$now] startinf pm2 service" >> DnA-backend/logs/backend.log
+
+pm2 start
