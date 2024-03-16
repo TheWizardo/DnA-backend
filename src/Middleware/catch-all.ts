a98150e5c73693a2f8a44fbb2ae4c65d286c66c4
TheWizard <oz.sabbag@gmail.com>
2024-03-16 21:35:50 +0200

    better parsing of messages

--- src/Middleware/catch-all.ts
+++ src/Middleware/catch-all.ts
@@ -3,20 +3,26 @@ import config from "../Utils/config";
 
 // catches all errors in the system and sends them as a response
 function catchAll(err: any, req: Request, res: Response, next: NextFunction): void {
-    console.log("error is:", err);
-    const statusCode = err.status ? err.status : (err.error?.status ? err.error.status : 500);
-    if (statusCode === 500 && config.environment === "production") {
-        err.error.message = "Something went wrong..."
+    try {
+        console.log("error is:", err);
+        const statusCode = err.status ? err.status : (err?.status ? err.status : 500);
+        if (statusCode === 500 && config.environment === "production") {
+            err.message = "Something went wrong..."
+        }
+        res.status(statusCode).send(extractErrorMessage(err));
     }
-    res.status(statusCode).send(err.error?.message);
+    catch(error: any) {
+        console.log(error);
+    }
 }
 
 function extractErrorMessage(err: any) {
     if (typeof err === "string") return err;
-    if (typeof err.response?.data === "string") return err.response.data;
-    if (Array.isArray(err.response?.data)) return err.response.data[0];
-    if (typeof err.message === "string") return err.message;
-    console.error(err);
+    if (typeof err?.error?.message === "string") return err.error.message;
+    if (typeof err?.response?.data === "string") return err.response.data;
+    if (Array.isArray(err?.response?.data)) return err.response.data[0];
+    if (typeof err?.message === "string") return err.message;
+    console.error("CatchAll", err);
     return "Oops...";
 }
 
