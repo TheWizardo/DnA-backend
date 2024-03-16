861715bc16ac5336ed8d513cc9014aa47d1c8be8
TheWizard <oz.sabbag@gmail.com>
2024-03-16 21:29:54 +0200

    removing unnecessary data from logs

--- src/Middleware/logger-mw.ts
+++ src/Middleware/logger-mw.ts
@@ -3,7 +3,25 @@ import { NextFunction, Request, Response } from "express";
 // mostly for debugging
 // logs incoming requests
 function logger(req: Request, res: Response, next: NextFunction): void {
-    console.log(`${req.method} ${req.baseUrl + req.originalUrl}`);
+    const urlParts = (req.baseUrl + req.originalUrl).split("?")
+    if (urlParts.length === 1) {
+        console.log(`${req.method} ${urlParts[0]}`);
+    }
+    else {
+        const url = [];
+        for (let u of urlParts[1].split("&")) {
+            if (!u.includes("privateKey")) {
+                url.push(u);
+            }
+        }
+        if (url.length === 0) {
+            console.log(`${req.method} ${urlParts[0]}`);
+        }
+        else {
+            
+        console.log(`${req.method} ${urlParts[0]}?${url.join("&")}`);
+        }
+    }
     next();
 }
 
