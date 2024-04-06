1f58b45cc247d77c1afcade94ad33f94055b7554
TheWizard <oz.sabbag@gmail.com>
2024-04-06 16:47:34 +0300

    changing permissions for the data files

--- update.sh
+++ update.sh
@@ -6,34 +6,33 @@ touch DnA-backend/logs/backend.log
 
 now=$(date +"%Y-%m-%dT%H:%M:%S")
 echo "[$now] deleteing typescript source" >> DnA-backend/logs/backend.log
-
 rm -rf DnA-backend/src
-
 now=$(date +"%Y-%m-%dT%H:%M:%S")
 echo "[$now] typescript source deleted" >> DnA-backend/logs/backend.log
 
 
 now=$(date +"%Y-%m-%dT%H:%M:%S")
 echo "[$now] changing log files permissions" >> DnA-backend/logs/backend.log
-
 chmod 666 DnA-backend/logs/backend.log
-
 now=$(date +"%Y-%m-%dT%H:%M:%S")
 echo "[$now] log files permissions chnaged" >> DnA-backend/logs/backend.log
 
+for file in "DnA-backend/build/Assets/data"/*; do
+    # Check if the file exists and is a regular file
+    if [[ -f "$file" ]]; then
+        # Change permissions to read and write for the user
+        chmod 666 "$file"
+    fi
+done
 
 now=$(date +"%Y-%m-%dT%H:%M:%S")
 echo "[$now] installing dependencies" >> DnA-backend/logs/backend.log
-
 cd DnA-backend/build
 npm i
-
 now=$(date +"%Y-%m-%dT%H:%M:%S")
 echo "[$now] dependencies installed" >> ../logs/backend.log
-
 cd ..
 cd ..
-
 now=$(date +"%Y-%m-%dT%H:%M:%S")
 echo "[$now] startinf pm2 service" >> DnA-backend/logs/backend.log
 
