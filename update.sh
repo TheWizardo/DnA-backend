4c679a413d2c9c45cee5cdf1b2fdd0ebeacd70e6
TheWizardo <39444976+TheWizardo@users.noreply.github.com>
2025-02-27 01:25:42 +0200

    updated script to run with docker

--- update.sh
+++ update.sh
@@ -1,47 +1,63 @@
-pm2 stop backend
+#!/usr/bin/env bash
 
-# copy data files to a temp folder.
+save="copy"
+datetime=$(date +"%Y-%m-%d_%H-%M-%S")
+mkdir -p logs
+if [ ! -f "logs/$datetime.log" ]; then
+    touch logs/$datetime.log
+fi
 
+now=$(date +"%F %T.%3N")
+echo "[$now] Stopping docker..." >> logs/$datetime.log
+cd DnA-backend
+docker-compose down >> ../logs/$datetime.log 2>&1
+cd ..
+
+# If the first argument is "copy", then back up data
+if [ "$1" == "$save" ]; then
+    now=$(date +"%F %T.%3N")
+    echo "[$now] Backing up data" >> logs/$datetime.log
+    cp -r ./DnA-backend/build/Assets/data ./temp
+fi
+
+now=$(date +"%F %T.%3N")
+echo "[$now] Deleting codebase" >> logs/$datetime.log
 rm -rf DnA-backend
-git clone https://github.com/TheWizardo/DnA-backend.git
-mkdir DnA-backend/logs
-touch DnA-backend/logs/backend.log
 
-now=$(date +"%Y-%m-%dT%H:%M:%S")
-echo "[$now] deleteing typescript source" >> DnA-backend/logs/backend.log
+now=$(date +"%F %T.%3N")
+echo "[$now] Downloading code from git" >> logs/$datetime.log
+git clone https://github.com/TheWizardo/DnA-backend.git >> logs/$datetime.log 2>&1
+
+now=$(date +"%F %T.%3N")
+echo "[$now] Deleting TypeScript source" >> logs/$datetime.log
 rm -rf DnA-backend/src
-now=$(date +"%Y-%m-%dT%H:%M:%S")
-echo "[$now] typescript source deleted" >> DnA-backend/logs/backend.log
 
+# If the first argument is "copy", then restore the data
+if [ "$1" == "$save" ]; then
+    now=$(date +"%F %T.%3N")
+    echo "[$now] Restoring data" >> logs/$datetime.log
+    rm -rf ./DnA-backend/build/Assets/data
+    cp -r ./temp ./DnA-backend/build/Assets/data
+    rm -rf ./temp
+fi
 
-now=$(date +"%Y-%m-%dT%H:%M:%S")
-echo "[$now] changing file permissions" >> DnA-backend/logs/backend.log
-chmod 666 DnA-backend/logs/backend.log
-now=$(date +"%Y-%m-%dT%H:%M:%S")
-echo "[$now] file permissions changed" >> DnA-backend/logs/backend.log
-
-# copy the files bac to the data folder
-# delete temp
-
-for file in "DnA-backend/build/Assets/data"/*; do
-    # Check if the file exists and is a regular file
+now=$(date +"%F %T.%3N")
+echo "[$now] Changing file permissions" >> logs/$datetime.log
+for file in DnA-backend/build/Assets/data/*; do
+    # Check if the file is a regular file
     if [[ -f "$file" ]]; then
         # Change permissions to read and write for the user
         chmod 666 "$file"
     fi
 done
 
-now=$(date +"%Y-%m-%dT%H:%M:%S")
-echo "[$now] installing dependencies" >> DnA-backend/logs/backend.log
-cd DnA-backend/build
-npm i
-now=$(date +"%Y-%m-%dT%H:%M:%S")
-echo "[$now] dependencies installed" >> ../logs/backend.log
-cd ..
-cd ..
-now=$(date +"%Y-%m-%dT%H:%M:%S")
-echo "[$now] startinf pm2 service" >> DnA-backend/logs/backend.log
+cd DnA-backend
+now=$(date +"%F %T.%3N")
+echo "[$now] Building docker..." >> ../logs/$datetime.log
+docker-compose build >> ../logs/$datetime.log 2>&1
 
-pm2 start
+now=$(date +"%F %T.%3N")
+echo "[$now] Starting docker..." >> ../logs/$datetime.log
+docker-compose up -d >> ../logs/$datetime.log 2>&1
 
-reboot
+echo "Full log is at ./logs/$datetime.log"
